3354cbf6d3f00371160b299f94afa1ca09833393
ricardo55991 <ricardo.barros.jr@gmail.com>
2023-05-24 10:03:20 -0300

    Criação estrutura básica

---
+++ ricardo_luana_matheusA/Domain/Domain/Controllers/ProdutoController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Domain.Controllers
+{
+    [RoutePrefix("api/produtos")]
+    public class ProdutoController : ApiController
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        [HttpPost]
+        public void Post(int id)
+        {
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public void Put(int id)
+        {
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
