a7a91d25148f568405e6ff8a2dea537fb3696fd5
rderoncio <rafael.deroncio@hotmail.com>
2023-05-24 13:38:38 -0300

    limpeza nas classes

--- Execricio.NETFramework.CRUD.Business/Models/ProdutoModel.cs
+++ Execricio.NETFramework.CRUD.Business/Models/ProdutoModel.cs
@@ -1,6 +1,5 @@
 ﻿using Execricio.NETFramework.CRUD.Business.DTOs;
 
-
 namespace Execricio.NETFramework.CRUD.Business.Models
 {
     public class ProdutoModel : ProdutoDTO
--- Execricio.NETFramework.CRUD.Business/Responses/PrecoResponse.cs
+++ Execricio.NETFramework.CRUD.Business/Responses/PrecoResponse.cs
@@ -1,9 +1,4 @@
 ﻿using Execricio.NETFramework.CRUD.Business.DTOs;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Execricio.NETFramework.CRUD.Business.Responses
 {
