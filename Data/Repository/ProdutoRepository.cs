984b8af570e291f0a38513c340accfef255c8cd4
Matheus Matos Moreira <matheus.matos@animaeducacao.com.br>
2023-05-24 15:15:22 -0300

    conexão com o banco

---
+++ Data/Repository/ProdutoRepository.cs
@@ -0,0 +1,21 @@
+﻿using Data.Context;
+using Data.Model;
+using Npgsql;
+
+namespace Data.Repository
+{
+    public class ProdutoRepository
+    {
+        public DataBaseConfig context = new DataBaseConfig();
+
+        //public Produto BuscaProduto(int id)
+        //{
+        //    using (var connection = new NpgsqlConnection(context.ConnectionString()))
+        //    {
+        //        connection.Open();
+        //        var sql = "SELECT * FROM clientes WHERE Id = @Id";
+        //        return connection.QueryFirstOrDefault<Produto>(sql, new { Id = id });
+        //    }
+        //}
+    }
+}
---
+++ Negocio/Service/ProdutoService.cs
@@ -0,0 +1,14 @@
+﻿using Data.Repository;
+
+namespace Negocio.Service
+{
+    public class ProdutoService
+    {
+        public ProdutoService()
+        {
+
+        }
+
+        public ProdutoRepository produtoRepository = new ProdutoRepository();
+    }
+}
