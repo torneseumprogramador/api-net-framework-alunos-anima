876c2cc9f025386f5f85c86f9ff4c5aa3ce04227
matheusaugusto61 <matheussouza.1802@aluno.unibh.br>
2023-05-26 11:05:52 -0300

    testando

---
+++ ricardoA_luana_matheusA/Domain/Database/DTO/PrecoDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.DTO
+{
+    public class PrecoDTO
+    {
+        public int Id { get; set; }
+        public string ProdutoId { get; set; }
+        public double Preco { get; set; }
+        public DateTime Data { get; set; }
+
+    }
+}
---
+++ ricardoA_luana_matheusA/Domain/Database/Interfaces/IPrecoRepository.cs
@@ -0,0 +1,18 @@
+﻿using Service.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Interfaces
+{
+    public interface IPrecoRepository
+    {
+        List<PrecoDTO> BuscarPrecos();
+        PrecoDTO BuscarPreco(int id);
+        bool InserirPreco(PrecoDTO preco);
+        bool AtualizarPreco(int id, PrecoDTO preco);
+        bool DeletarPreco(int id);
+    }
+}
