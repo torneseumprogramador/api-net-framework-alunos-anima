8d265495c5e30fcda1da9af8d2489d515f94854c
gabrieldev-7cd <gabriel.alexandre.dev@gmail.com>
2023-05-26 00:21:19 -0300

    Crud de precoProduto e collection completa da api

--- AndersonCairoGabriel/BaseDados/Repositories/GenericRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿
-namespace BaseDados.Repositories
-{
-    public class GenericRepository
-    {
-
-    }
-}
--- AndersonCairoGabriel/Controllers/PrecoProdutoController.cs
+++ AndersonCairoGabriel/Controllers/PrecoProdutoController.cs
@@ -1,19 +1,56 @@
 ﻿using Entidades;
+using Entidades.DTOs;
 using Negocios;
-using System.Collections.Generic;
 using System.Net;
 using System.Web.Http;
-using System.Web.Mvc;
 
 namespace AndersonCairoGabriel.Controllers
 {
     public class PrecoProdutoController : ApiController
     {
-        private readonly Produtos _produtosBusiness;
+        private readonly PrecoProdutos _precoProdutoBusiness;
 
         public PrecoProdutoController()
         {
-            _produtosBusiness = new Produtos();
+            _precoProdutoBusiness = new PrecoProdutos();
         }
+
+        [HttpGet]
+        public IHttpActionResult ListarPessoas()
+        {
+            var produtos = _precoProdutoBusiness.ListarPrecoProduto();
+            return Ok(produtos);
+        }
+
+        [HttpGet]
+        public IHttpActionResult ObterPrecoProduto(int id)
+        {
+            var produto = _precoProdutoBusiness.ObterPrecoProduto(id);
+
+            return Ok(produto);
+        }
+
+
+        [HttpPost]
+        public IHttpActionResult CadastrarProduto([FromBody] PrecoProduto precoProduto)
+        {
+            var novoProduto = _precoProdutoBusiness.CadastrarPrecoProduto(precoProduto);
+            return Ok(novoProduto);
+        }
+
+        [HttpPut]
+        public IHttpActionResult AtualizarProduto(int id, [FromBody] PrecoProduto precoProduto)
+        {
+            var produtoAtualizado = _precoProdutoBusiness.AtualizarPrecoProduto(id, precoProduto);
+            return Ok(produtoAtualizado);
+        }
+
+        [HttpDelete]
+        public IHttpActionResult ExluirProduto(int id)
+        {
+            _precoProdutoBusiness.ExcluirPrecoProduto(id);
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
     }
 }
