215e39b8c56982dd8e6098c05060b880e9508599
ricardo55991 <ricardo.barros.jr@gmail.com>
2023-05-25 22:14:57 -0300

    Ajustes na nomenclatura, ajuste na estrutura e  finalização dos endpoints

---
+++ ricardoA_luana_matheusA/Domain/Database/Repositories/ProdutoRepository.cs
@@ -0,0 +1,160 @@
+﻿using Repository.Interfaces;
+using Service.DTO;
+using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Repository
+{
+    public class ProdutoRepository : IProdutoRepository
+    {
+        string connectionString = ("Data Source=" + Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data", "reserva.db") + ";Version=3;").Replace("\\UI\\", "\\Database\\");
+
+        public List<ProdutoDTO> BuscarProdutos()
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                var sql = @"select
+                                pro.id Id,
+                                pro.nome Nome,
+                                pro.descricao Descricao,
+                                pre.preco Valor,
+                                pre.data Data
+                            from
+                                produto pro
+                            join preco pre on
+                                pre.produto_id = pro.id
+                            group by pro.id , pro.nome, pro.descricao
+                            having max(pre.data)";
+
+                connection.Open();
+                using (SQLiteCommand command = new SQLiteCommand(sql, connection))
+                {
+                    using (var reader = command.ExecuteReader())
+                    {
+                        var produtos = new List<ProdutoDTO>();
+                        while (reader.Read())
+                        {
+                            var produto = new ProdutoDTO
+                            {
+                                Id = Convert.ToInt32(reader["Id"]),
+                                Nome = reader["Nome"].ToString(),
+                                Descricao = reader["Descricao"].ToString(),
+                                Valor = Convert.ToDouble(reader["Valor"]),
+                                Data = Convert.ToDateTime(reader["Data"].ToString())
+                            };
+                            produtos.Add(produto);
+                        }
+                        return produtos;
+                    }
+
+                }
+            }
+        }
+
+        public ProdutoDTO BuscarProduto(int id)
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                var sql = @"SELECT
+                                pro.id AS Id,
+                                pro.nome AS Nome,
+                                pro.descricao AS Descricao,
+                                pre.preco AS Valor,
+                                pre.data AS Data
+                            FROM
+                                produto pro
+                            JOIN
+                                preco pre ON pre.produto_id = pro.id
+                            WHERE
+                                pro.id = @id";
+
+                connection.Open();
+                using (SQLiteCommand command = new SQLiteCommand(sql, connection))
+                {
+                    command.Parameters.AddWithValue("@id", id);
+                    using (var reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            var produto = new ProdutoDTO
+                            {
+                                Id = Convert.ToInt32(reader["Id"]),
+                                Nome = reader["Nome"].ToString(),
+                                Descricao = reader["Descricao"].ToString(),
+                                Valor = Convert.ToDouble(reader["Valor"]),
+                                Data = Convert.ToDateTime(reader["Data"].ToString())
+                            };
+                            return produto;
+                        }
+                        return null;
+                    }
+                }
+            }
+        }
+
+        public bool InserirProduto(ProdutoDTO produto)
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+                using (SQLiteCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = @"INSERT INTO produto (nome, descricao) VALUES (@nome, @descricao);
+                                            INSERT INTO preco (produto_id, preco, data) VALUES (last_insert_rowid(), @valor, datetime('now', 'localtime'));";
+                    command.Parameters.AddWithValue("@nome", produto.Nome);
+                    command.Parameters.AddWithValue("@descricao", produto.Descricao);
+                    command.Parameters.AddWithValue("@valor", produto.Valor);
+
+                    return command.ExecuteNonQuery() > 0;
+                }
+            }
+        }
+
+        public bool AtualizarProduto(int id, ProdutoDTO produto)
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+                using (SQLiteCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = @"UPDATE produto
+                                            SET nome = @nome, descricao = @descricao
+                                            WHERE id = @id;
+                                            UPDATE preco
+                                            SET preco = @valor, data = datetime('now', 'localtime')
+                                            WHERE produto_id = @id;";
+                    command.Parameters.AddWithValue("@id", id);
+                    command.Parameters.AddWithValue("@nome", produto.Nome);
+                    command.Parameters.AddWithValue("@descricao", produto.Descricao);
+                    command.Parameters.AddWithValue("@valor", produto.Valor);
+
+                    return command.ExecuteNonQuery() > 0;
+                }
+            }
+        }
+
+
+        public bool DeletarProduto(int id)
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+                using (SQLiteCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = @"DELETE FROM produto WHERE id = @id;
+                                        DELETE FROM preco WHERE produto_id = @id;";
+                    command.Parameters.AddWithValue("@id", id);
+
+                    return command.ExecuteNonQuery() > 0;
+                }
+            }
+        }
+    }
+}
+
---
+++ ricardoA_luana_matheusA/Domain/UI/Controllers/ProdutoController.cs
@@ -0,0 +1,86 @@
+﻿using Service.DTO;
+using Service.Services;
+using Service.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Results;
+
+namespace Domain.Controllers
+{
+    [RoutePrefix("api")]
+    public class ProdutoController : ApiController
+    {
+        private readonly IProdutoService _produtoService;
+
+        public ProdutoController()
+        {
+            _produtoService = new ProdutoService();
+        }
+
+        [HttpGet]
+        [Route("produtos")]
+        public List<ProdutoDTO> BuscarProdutos()
+        {
+            return _produtoService.BuscarProdutos();
+        }
+
+        [HttpGet]
+        [Route("produtos/{id}")]
+        public ProdutoDTO BuscarProduto(int id)
+        {
+            return _produtoService.BuscarProduto(id);
+        }
+
+        [HttpPost]
+        [Route("produtos")]
+        public string InserirProduto(ProdutoDTO produto)
+        {
+            var result = _produtoService.InserirProduto(produto);
+            
+            if(result == true)
+            {
+                return "Produto cadastrado com sucesso!";
+            }
+            else
+            {
+                return "Erro ao cadastrar produto!";
+            }
+        }
+
+        [HttpPut]
+        [Route("produtos/{id}")]
+        public string AtualizarProduto(int id, ProdutoDTO produto)
+        {
+            var result = _produtoService.AtualizarProduto(id, produto);
+
+            if (result == true)
+            {
+                return "Produto atualizado com sucesso!";
+            }
+            else
+            {
+                return "Erro ao atualizar produto!";
+            }
+        }
+
+        [HttpDelete]
+        [Route("produtos/{id}")]
+        public string DeletarProduto(int id)
+        {
+            var result = _produtoService.DeletarProduto(id);
+
+            if (result == true)
+            {
+                return "Produto deletado com sucesso!";
+            }
+            else
+            {
+                return "Erro ao deletar produto!";
+            }
+        }
+    }
+}
